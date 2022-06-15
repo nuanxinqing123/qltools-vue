3a02c20f3158a9b73433fe7cc22573734db608f6
fanjunwei <729927758@qq.com>
2022-06-15 17:14:12 +0800

    路由守卫拦截，更改title

--- src/router/index.js
+++ src/router/index.js
@@ -1,143 +1,185 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {
+	createRouter,
+	createWebHashHistory
+} from 'vue-router'
 import axios from "axios";
-
-const routes = [
-  {
-    path: '/',
-    name: 'Index',
-    meta: {
-      requireAuth: false,
-      title: '首页'
-    },
-    component: () => import('../views/IndexView.vue'),
-  },
-  {
-    path: '/login',
-    name: 'login',
-    meta: {
-      requireAuth: false,
-      title: '管理员登录'
-    },
-    component: () => import('../views/SignView/LoginView')
-  },
-  {
-    path: '/register',
-    name: 'register',
-    meta: {
-      requireAuth: false,
-      title: '管理员注册'
-    },
-    component: () => import('../views/SignView/RegisterView')
-  },
-  {
-    path: '/admin',
-    name: 'admin',
-    meta: {
-      requireAuth: true,
-      title: '后台管理'
-    },
-    component: () => import('../views/Admin/AdminView'),
-  },
-  {
-    path: '/admin/env',
-    name: 'adminEnv',
-    meta: {
-      requireAuth: true,
-      title: '变量管理'
-    },
-    component: () => import('../views/Admin/AdminEnvView'),
-  },
-  {
-    path: '/admin/panel',
-    name: 'adminPanel',
-    meta: {
-      requireAuth: true,
-      title: '面板管理'
-    },
-    component: () => import('../views/Admin/AdminPanelView'),
-  },
-  {
-    path: '/admin/plugin',
-    name: 'adminPlugin',
-    meta: {
-      requireAuth: true,
-      title: '插件管理'
-    },
-    component: () => import('../views/Admin/AdminPluginView'),
-  },
-  {
-    path: '/admin/transfer',
-    name: 'adminTransfer',
-    meta: {
-      requireAuth: true,
-      title: '容器管理'
-    },
-    component: () => import('../views/Admin/AdminTransferView'),
-  },
-  {
-    path: '/admin/cdk',
-    name: 'adminCD-KEY',
-    meta: {
-      requireAuth: true,
-      title: 'CD-KEY管理'
-    },
-    component: () => import('../views/Admin/AdminAboutCDK'),
-  },
-  {
-    path: '/admin/message',
-    name: 'adminMessage',
-    meta: {
-      requireAuth: true,
-      title: '推送管理'
-    },
-    component: () => import('../views/Admin/AdminMsgView'),
-  },
-  {
-    path: '/admin/settings',
-    name: 'adminSettings',
-    meta: {
-      requireAuth: true,
-      title: '网站设置'
-    },
-    component: () => import('../views/Admin/AdminSettingsView'),
-  },
-  {
-    path: '/admin/about',
-    name: 'adminAbout',
-    meta: {
-      requireAuth: true,
-      title: '关于面板'
-    },
-    component: () => import('../views/Admin/AdminAboutView'),
-  }
+const routes = [{
+		path: '/',
+		name: 'Index',
+		meta: {
+			requireAuth: false,
+			title: '首页'
+		},
+		component: () => import('../views/IndexView.vue'),
+		beforeEnter: () => {
+			axios.get("/v2/api/set/settings").then((res) => {//将接口赋予的值给title
+					window.document.title = res.data.data[5].value
+			})
+		}
+	},
+	{
+		path: '/login',
+		name: 'login',
+		meta: {
+			requireAuth: false,
+			title: '管理员登录'
+		},
+		component: () => import('../views/SignView/LoginView'),
+		beforeEnter: () => {
+			window.document.title = "管理员登录"
+		}
+	},
+	{
+		path: '/register',
+		name: 'register',
+		meta: {
+			requireAuth: false,
+			title: '管理员注册'
+		},
+		component: () => import('../views/SignView/RegisterView'),
+		beforeEnter: () => {
+			window.document.title = "管理员注册"
+		}
+	},
+	{
+		path: '/admin',
+		name: 'admin',
+		meta: {
+			requireAuth: true,
+			title: '后台管理'
+		},
+		component: () => import('../views/Admin/AdminView'),
+		beforeEnter: () => {
+			window.document.title = "后台管理"
+		}
+	},
+	{
+		path: '/admin/env',
+		name: 'adminEnv',
+		meta: {
+			requireAuth: true,
+			title: '变量管理'
+		},
+		component: () => import('../views/Admin/AdminEnvView'),
+		beforeEnter: () => {
+			window.document.title = "变量管理"
+		}
+	},
+	{
+		path: '/admin/panel',
+		name: 'adminPanel',
+		meta: {
+			requireAuth: true,
+			title: '面板管理'
+		},
+		component: () => import('../views/Admin/AdminPanelView'),
+		beforeEnter: () => {
+			window.document.title = "面板管理"
+		}
+	},
+	{
+		path: '/admin/plugin',
+		name: 'adminPlugin',
+		meta: {
+			requireAuth: true,
+			title: '插件管理'
+		},
+		component: () => import('../views/Admin/AdminPluginView'),
+		beforeEnter: () => {
+			window.document.title = "插件管理"
+		}
+	},
+	{
+		path: '/admin/transfer',
+		name: 'adminTransfer',
+		meta: {
+			requireAuth: true,
+			title: '容器管理'
+		},
+		component: () => import('../views/Admin/AdminTransferView'),
+		beforeEnter: () => {
+			window.document.title = "容器管理"
+		}
+	},
+	{
+		path: '/admin/cdk',
+		name: 'adminCD-KEY',
+		meta: {
+			requireAuth: true,
+			title: 'CD-KEY管理'
+		},
+		component: () => import('../views/Admin/AdminAboutCDK'),
+		beforeEnter: () => {
+			window.document.title = "CD-KEY管理"
+		}
+	},
+	{
+		path: '/admin/message',
+		name: 'adminMessage',
+		meta: {
+			requireAuth: true,
+			title: '推送管理'
+		},
+		component: () => import('../views/Admin/AdminMsgView'),
+		beforeEnter: () => {
+			window.document.title = "推送管理"
+		}
+	},
+	{
+		path: '/admin/settings',
+		name: 'adminSettings',
+		meta: {
+			requireAuth: true,
+			title: '网站设置'
+		},
+		component: () => import('../views/Admin/AdminSettingsView'),
+		beforeEnter: () => {
+			window.document.title = "网站设置"
+		}
+	},
+	{
+		path: '/admin/about',
+		name: 'adminAbout',
+		meta: {
+			requireAuth: true,
+			title: '关于面板'
+		},
+		component: () => import('../views/Admin/AdminAboutView'),
+		beforeEnter: () => {
+			window.document.title = "关于面板"
+		}
+	}
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
-  routes
+	
+	history: createWebHashHistory(process.env.BASE_URL),
+	routes
 })
 
-router.beforeEach((to, from, next) => {
-  if(to.meta.requireAuth){
-    let Token = localStorage.getItem('Bearer')
-    if (Token === null || Token === ""){
-      router.push('login')
-    } else {
-      let TokenData = {"token": localStorage.getItem('Bearer')};
-      // 检查Token是否有效
-      axios.post("/v1/api/check/token", TokenData).then((res) => {
-        if (res.data.data === true) {
-          next()
-        } else {
-          // Token失效
-          localStorage.clear()
-          router.push('login')
-        }
-      })
-    }
-  } else {
-    next()
-  }
+router.beforeEach((to, from, next) => { //to进来是这个，from带来的值,next即将去的路由
+	if (to.meta.requireAuth) {
+		let Token = localStorage.getItem('Bearer')
+		if (Token === null || Token === "") {
+			router.push('login')
+		} else {
+			let TokenData = {
+				"token": localStorage.getItem('Bearer')
+			};
+			// 检查Token是否有效
+			axios.post("/v1/api/check/token", TokenData).then((res) => {
+				if (res.data.data === true) {
+					next()
+				} else {
+					// Token失效
+					localStorage.clear()
+					router.push('login')
+				}
+			})
+		}
+	} else {
+		next()
+	}
 })
 
 export default router
